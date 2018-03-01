9227bd9a1cc3494b40b2113612d4b82810e5f6ba
Katerina Bezlepkina <ekaterinabez@gmail.com>
2018-03-01 10:18:32 +0800

    sort Selector refactor, tooltip style fixes, changed box-layout to grid

--- src/components/common/Tooltip.js
+++ src/components/common/Tooltip.js
@@ -2,30 +2,12 @@ import React, { Fragment } from 'react';
 
 const Tooltip = ({ isHidden, offSetTop, text }) => {
   const tooltip = {
-    position: 'absolute',
-    background: 'rgb(155, 154, 154)',
-    borderRadius: '3px',
-    marginLeft: '40px',
-    padding: '1.2rem 1.6rem',
-    textAlign: 'center',
-    width: '10rem',
-    top: `${offSetTop - 60}px`,
-    transition: '0.3s ease-in-out',
-    ':after': {
-      content: ' ',
-      position: 'absolute',
-      top: '100%', /* At the bottom of the tooltip */
-      left: '50%',
-      marginLeft: '-5px',
-      borderWidth: '5px',
-      borderStyle: 'solid',
-      borderColor: 'black transparent transparent transparent',
-    },
+    top: `${offSetTop - 80}px`,
   };
 
   return (
     <Fragment>
-      { !isHidden && <div style={tooltip}><p>{text}</p></div> }
+      { !isHidden && <div style={tooltip} className="tooltip"><p>{text}</p></div> }
     </Fragment>
   );
 };
